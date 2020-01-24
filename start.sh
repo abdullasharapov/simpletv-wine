acb8637d2eb65746f3e0fd7406997b5f64e2c916
abdullasharapov <abdullasharapov@gmail.com>
2020-01-24 21:26:01 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /usr/bin/Xvfb :20.0 -ac -screen 0 800x600x24 > /dev/null 2>&1 & 
-DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.17.0.2','10000')" > /dev/null 2>&1 &
-(sleep 30 && DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.17.0.2','10002')") > /dev/null 2>&1 &
-(sleep 60 && DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.17.0.2','10004')") > /dev/null 2>&1
+DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.18.0.2','10000')" > /dev/null 2>&1 &
+(sleep 30 && DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.18.0.2','10002')") > /dev/null 2>&1 &
+(sleep 60 && DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.18.0.2','10004')") > /dev/null 2>&1
