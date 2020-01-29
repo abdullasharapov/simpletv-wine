cb468af8add12d1eb6f5a1581c304530e73cdf70
abdullasharapov <abdullasharapov@gmail.com>
2020-01-29 15:39:32 +0300

    Remove REG ADD from start script

--- start_stv.sh
+++ start_stv.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#/opt/wine-stable/bin/wine reg ADD "HKLM\\System\\CurrentControlSet\\Control\\Session Manager\\Environment" /v "PATH" /t REG_SZ /d "c:\\windows;c:\\windows\\system32" &
-/usr/bin/Xvfb :20.0 -ac -screen 0 800x600x24 & 
+/usr/bin/Xvfb :20.0 -ac -screen 0 800x600x24 &
 DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('192.168.10.172','10000')"
-#(sleep 30 && DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.18.0.2','10002')") > /dev/null 2>&1 &
-#(sleep 60 && DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.18.0.2','10004')") > /dev/null 2>&1
+#(sleep 30 && DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.18.0.2','10002')") & #Second instance
+#(sleep 60 && DISPLAY=:20.0 wine "/root/.wine/drive_c/simpleTV/tv.exe" -nooneinstance -execute "loadfile(m_simpleTV.MainScriptDir .. 'user/TVSources/core/x64/tvs_server.lua')('172.18.0.2','10004')")   #Third instance
